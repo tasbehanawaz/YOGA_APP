4538ed2e6bfda1be8cdfbd4f8dcdea666a2dbc24
Justice <unankajustice@gmail.com>
2024-06-18 10:43:42 +0100

    feat: Update favicon link in index.html and replace carousel images in carousel.jsx

--- frontend/src/components/carousel/carousel.jsx
+++ frontend/src/components/carousel/carousel.jsx
@@ -1,11 +1,18 @@
 import { Carousel } from '@material-tailwind/react';
-import yogaPose from '../../assets/carl-barcelo-nqUHQkuVj3c-unsplash.jpg';
-import yogaPose1 from '../../assets/oksana-taran-xB4ExGcUai0-unsplash.jpg';
-import yogaPose2 from '../../assets/zen-bear-yoga-IVf7hm88zxY-unsplash.jpg';
+import yogaPose from '../../assets/image1.jpg';
+import yogaPose1 from '../../assets/image3.jpg';
+import yogaPose2 from '../../assets/image4.jpg';
+import yogaPose3 from '../../assets/image2.jpg';
+import yogaPose4 from '../../assets/image5.jpg';
 
 export function CarouselDefault() {
   return (
-    <Carousel className="rounded-xl">
+    <Carousel
+      className="rounded-xl"
+      autoplay={true}
+      loop={true}
+      autoplayDelay={3000}
+    >
       <img
         src={yogaPose}
         alt="lady doing yoga pose"
@@ -18,6 +25,16 @@ export function CarouselDefault() {
       />
       <img
         src={yogaPose1}
+        alt="woman doing yoga"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src={yogaPose3}
+        alt="woman doing yoga"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src={yogaPose4}
         alt="woman doing yoga"
         className="h-full w-full object-cover"
       />
