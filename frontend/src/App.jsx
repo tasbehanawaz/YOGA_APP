cfaf59332e57d9f2d802a494170d5655d04556cc
Tasbeha Nawaz <tasbehanawaz@gmail.com>
2024-06-22 20:57:53 +0100

    added the welcome page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,29 @@
 // src/App.jsx
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import Routes and Route
-import Program from './Pages/Program/program';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/home';
 import Layout from './components/layout/layout';
 import Categories from './Pages/categories/categories';
-import Login from './Pages/login/logins';
+import Register from './Pages/login/logins'; // Correct default import
 import Result from './Pages/result/result';
 import SignIn from './components/signIn/signIn';
-// import ButtonWithLink from './components/ButtonWithLink'; // Ensure this file exists
-import Sequence from './Pages/sequence/sequence'; // Import the Sequence component
+import Sequence from './Pages/sequence/sequence';
 import Profile from './Pages/profile/profile';
 import Save from './Pages/saved/savedPoses';
+// import Welcome from './Pages/welcome/welcome';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
+        {/* Welcome page route
+        <Route path="/welcome" element={<Welcome />} /> */}
+
+      <Route
           path="/"
           element={
             <Layout>
-              <Program />
+              <Home />
             </Layout>
           }
         />
@@ -37,7 +40,7 @@ function App() {
           path="/logins"
           element={
             <Layout>
-              <Login />
+              <Register />
             </Layout>
           }
         />
@@ -56,8 +59,7 @@ function App() {
               <Result />
             </Layout>
           }
-          />
-
+        />
         <Route
           path="/profile"
           element={
