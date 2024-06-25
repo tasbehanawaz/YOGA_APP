23b95577e3f3c97f08c116ea830974f1693aeb34
Tasbeha Nawaz <tasbehanawaz@gmail.com>
2024-06-26 00:19:42 +0100

    fixed generate button issue

--- frontend/src/Pages/Generate/generate.jsx
+++ frontend/src/Pages/Generate/generate.jsx
@@ -1,26 +1,25 @@
 import  { useState } from 'react';
 import axios from 'axios';
+// import { Sequence } from './sequence/sequence';
 
 
 
 const VideoGenerator = () => {
-  const [selectedPoses] = useState([]); // Assuming an initial state for selectedPoses
-  const [videoUrl, setVideoUrl] = useState('');
-  const [loading, setLoading] = useState(false);
+// eslint-disable-next-line no-unused-vars
+const [selectedPoses, setSelectedPoses] = useState([]); // Assuming an initial state for selectedPoses
+const [videoUrl, setVideoUrl] = useState('');
+const [loading, setLoading] = useState(false);
 
-  const handleGenerateVideo = () => {
-  setLoading(true);
-  axios.post('http://localhost:8001/yoga_sequence.mp4', { poses: selectedPoses })
-    .then((response) => {
-      setLoading(false);
-      if (response.data.videoPath) {
-        setVideoUrl(response.data.videoPath);
-      } else {
-        console.error('Error generating video:', response.data.error);
+const handleGenerateVideo = () => {setLoading(true);
+ axios.post('http://localhost:8001/yoga_sequence.mp4', { poses: selectedPoses }).then((response) => {
+  setLoading(false);
+   if (response.data.videoPath) {
+     setVideoUrl(response.data.videoPath);
+    } else {
+       console.error('Error generating video:', response.data.error);
         alert('Failed to generate video.');
-      }
-    })
-    .catch((error) => {
+     }
+    }).catch((error) => {
       setLoading(false);
       console.error('Error generating video:', error);
       alert('Error generating video.');
@@ -29,13 +28,12 @@ const VideoGenerator = () => {
 
  return (
   <div>
-    <button onClick={handleGenerateVideo} disabled={loading}>
-      {loading ? 'Generating...' : 'Generate Video'}
-    </button>
+   <button onClick={handleGenerateVideo} disabled={loading}>
+    {loading ? 'Generating...' : 'Generate Video'}
+   </button>
     {videoUrl && (
-      <video src={videoUrl} controls onError={() => alert('Error loading video.')} />
-    )}
-  </div>
+     <video src={videoUrl} controls onError={() => alert('Error loading video.')} />)}
+     </div>
 );
 };
 
